394e8e472266f4d5229785f6e9b50ef959f6dfc5
mariaclarabarbosa <maria.estrella@neki-it.com.br>
2021-07-07 08:32:20 -0300

    Criação do projeto

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "auto",
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all"
+};
